3a75915fff7e81cdbb24122e359a58e688958d13
stuartj073 <stuartj073@gmail.com>
2021-10-14 09:33:15 +0000

    Add image directory to workspace along with landing image

--- app.py
+++ app.py
@@ -1,27 +1,63 @@
 import os
-from flask import (Flask, render_template, redirect, request, flash,
-                    session, url_for)
-from bson.objectid import ObjectId
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
-    import env.py
+    import env
 
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"]= os.environ.get("MONGO_URI")
-app.config["MONGODB_NAME"]= os.environ.get("MONGODB_NAME")
-app.config["SECRET_KEY"]= os.environ.get("SECRET_KEY")
+app.config["MONGODB_NAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
 
-mongo= PyMongo(app)
+
+mongo = PyMongo(app)
+
+
+@app.route("/")
+@app.route("/landing")
+def landing():
+    return render_template("landing.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check if user is registered
+        existing_user = mongo.db.users.find_one(
+        {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Apologies, user is already registered")
+            return redirect(url_for("register"))
+        
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": genereate_password_has(request.form.get("password"))
+        }
+
+        mongo.db.users.insert_one(register)
+
+        # put new user into session cookie
+
+        session["user"] = request.form.get("username").lower()
+        flash("Registration successful")
+        return redirect(url_for("profile", username = session["user"]))
+
+    return render_template("register.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")), 
         debug=True)
-
-@app.route("/")
-@app.route("/landing")
-def landing():
-    return render_template("landing.html")
